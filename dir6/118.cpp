3c2c813d0db548e2691aaa68b75f1a446148e039
raunak1101 <134578765+raunak1101@users.noreply.github.com>
2023-06-28 15:24:08 +0530

    Update 118.cpp

--- dir6/118.cpp
+++ dir6/118.cpp
@@ -1,34 +1,108 @@
-#include<bits/stdc++.h>
+// A C++ program to print topological
+// sorting of a DAG
+#include <bits/stdc++.h>
 using namespace std;
 
+// Class to represent a graph
+class Graph {
+	// No. of vertices'
+	int V;
 
-#define ll long long
-#define endl '\n'
-#define debug(n) cout<<(n)<<endl;
-const ll INF = 2e18 + 99;
+	// Pointer to an array containing adjacency listsList
+	list<int>* adj;
 
-int main(){
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+	// A function used by topologicalSort
+	void topologicalSortUtil(int v, bool visited[],
+							stack<int>& Stack);
 
-  int t;
-  cin>>t;
-  while(t--){
-    int n;
-    cin>>n;
-    int eve = 0, odd = 0;
-    int x;
-    while(n--){
-      cin>>x;
-      if(x % 2 == 0){
-        eve++;
-      }
-      else{
-        odd++;
-      }
-    }
+public:
+	// Constructor
+	Graph(int V);
 
-    cout<<min(odd, eve)<<endl;
-  }
+	// function to add an edge to graph
+	void addEdge(int v, int w);
 
+	// prints a Topological Sort of
+	// the complete graph
+	void topologicalSort();
+};
+
+Graph::Graph(int V)
+{
+	this->V = V;
+	adj = new list<int>[V];
+}
+
+void Graph::addEdge(int v, int w)
+{
+	// Add w to v’s list.
+	adj[v].push_back(w);
+}
+
+// A recursive function used by topologicalSort
+void Graph::topologicalSortUtil(int v, bool visited[],
+								stack<int>& Stack)
+{
+	// Mark the current node as visited.
+	visited[v] = true;
+
+	// Recur for all the vertices
+	// adjacent to this vertex
+	list<int>::iterator i;
+	for (i = adj[v].begin(); i != adj[v].end(); ++i)
+		if (!visited[*i])
+			topologicalSortUtil(*i, visited, Stack);
+
+	// Push current vertex to stack
+	// which stores result
+	Stack.push(v);
+}
+
+// The function to do Topological Sort.
+// It uses recursive topologicalSortUtil()
+void Graph::topologicalSort()
+{
+	stack<int> Stack;
+
+	// Mark all the vertices as not visited
+	bool* visited = new bool[V];
+	for (int i = 0; i < V; i++)
+		visited[i] = false;
+
+	// Call the recursive helper function
+	// to store Topological
+	// Sort starting from all
+	// vertices one by one
+	for (int i = 0; i < V; i++)
+		if (visited[i] == false)
+			topologicalSortUtil(i, visited, Stack);
+
+	// Print contents of stack
+	while (Stack.empty() == false) {
+		cout << Stack.top() << " ";
+		Stack.pop();
+	}
+	
+	delete [] visited;
+}
+
+// Driver Code
+int main()
+{
+	// Create a graph given in the above diagram
+	Graph g(6);
+	g.addEdge(5, 2);
+	g.addEdge(5, 0);
+	g.addEdge(4, 0);
+	g.addEdge(4, 1);
+	g.addEdge(2, 3);
+	g.addEdge(3, 1);
+
+	cout << "Following is a Topological Sort of the given "
+			"graph \n";
+
+	// Function Call
+	g.topologicalSort();
+
+	return 0;
 }
