7d566348b3bd53df1b9b5a69765720f1cd3f8da1
Raunak Somani <raunaksomani321@gmail.com>
2023-06-28 15:18:56 +0530

    Update 44.cpp

--- dir3/44.cpp
+++ dir3/44.cpp
@@ -1,41 +1,103 @@
+// C++ program to find minimum average
+// weight of a cycle in connected and
+// directed graph.
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+const int V = 4;
 
-  int t;
-  cin>>t;
-  long long n;
-  while(t--){
-    cin>>n;
-    int cnt3 = 0, cnt2 = 0;
-    int k = n;
-    while(k % 3 == 0 && k > 1){
-      k /= 3;
-      cnt3++;
-    }
-    while(k % 2 == 0 && k > 1){
-      k /= 2;
-      cnt2++;
-    }
-    if(k != 1){
-      cout<<-1<<endl;
-      continue;
-    }
-    if(cnt3 < cnt2){
-      cout<<-1<<endl;
-      continue;
-    }
-    int cnt6 = cnt3 - cnt2;
-    n *= pow(2, cnt6);
-    while(n>1){
-      n /= 6;
-      cnt6++;
-    }
-    cout<<cnt6<<endl;
-    continue;
-  }
-  return 0;
+// a struct to represent edges
+struct edge
+{
+	int from, weight;
+};
+
+// vector to store edges
+vector <edge> edges[V];
+
+void addedge(int u,int v,int w)
+{
+	edges[v].push_back({u, w});
+}
+
+// calculates the shortest path
+void shortestpath(int dp[][V])
+{
+	// initializing all distances as -1
+	for (int i=0; i<=V; i++)
+		for (int j=0; j<V; j++)
+			dp[i][j] = -1;
+
+	// shortest distance from first vertex
+	// to in itself consisting of 0 edges
+	dp[0][0] = 0;
+
+	// filling up the dp table
+	for (int i=1; i<=V; i++)
+	{
+		for (int j=0; j<V; j++)
+		{
+			for (int k=0; k<edges[j].size(); k++)
+			{
+				if (dp[i-1][edges[j][k].from] != -1)
+				{
+					int curr_wt = dp[i-1][edges[j][k].from] +
+								edges[j][k].weight;
+					if (dp[i][j] == -1)
+						dp[i][j] = curr_wt;
+					else
+					dp[i][j] = min(dp[i][j], curr_wt);
+				}
+			}
+		}
+	}
+}
+
+// Returns minimum value of average weight of a
+// cycle in graph.
+double minAvgWeight()
+{
+	int dp[V+1][V];
+	shortestpath(dp);
+
+	// array to store the avg values
+	double avg[V];
+	for (int i=0; i<V; i++)
+		avg[i] = -1;
+
+	// Compute average values for all vertices using
+	// weights of shortest paths store in dp.
+	for (int i=0; i<V; i++)
+	{
+		if (dp[V][i] != -1)
+		{
+			for (int j=0; j<V; j++)
+				if (dp[j][i] != -1)
+					avg[i] = max(avg[i],
+				((double)dp[V][i]-dp[j][i])/(V-j));
+		}
+	}
+
+	// Find minimum value in avg[]
+	double result = avg[0];
+	for (int i=0; i<V; i++)
+		if (avg[i] != -1 && avg[i] < result)
+			result = avg[i];
+
+	return result;
+}
+
+// Driver function
+int main()
+{
+	addedge(0, 1, 1);
+	addedge(0, 2, 10);
+	addedge(1, 2, 3);
+	addedge(2, 3, 2);
+	addedge(3, 1, 0);
+	addedge(3, 0, 8);
+
+	cout << minAvgWeight();
+
+	return 0;
 }
