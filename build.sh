4e5e3973468aa72b78657feffdff8d25ed81aff8
raunak1101 <134578765+raunak1101@users.noreply.github.com>
2023-06-30 11:37:32 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Compile a.c
+
+
+
+for file in /users/ses/rs_14/git/InternProject/dir2/*.c; do
+    if [ -f "$file" ]; then
+        gcc "$file" -o "${file%.*}"
+    fi
+done
+for file in /users/ses/rs_14/git/InternProject/dir3/*.c; do
+    if [ -f "$file" ]; then
+        gcc "$file" -o "${file%.*}"
+    fi
+done
+for file in /users/ses/rs_14/git/InternProject/dir4/*.c; do
+    if [ -f "$file" ]; then
+        gcc "$file" -o "${file%.*}"
+    fi
+done
+for file in /users/ses/rs_14/git/InternProject/dir5/*.c; do
+    if [ -f "$file" ]; then
+        gcc "$file" -o "${file%.*}"
+    fi
+done
+for file in /users/ses/rs_14/git/InternProject/dir6/*.c; do
+    if [ -f "$file" ]; then
+        gcc "$file" -o "${file%.*}"
+    fi
+done
+
+for file in /users/ses/rs_14/git/InternProject/dir2/*.cpp; do
+    if [ -f "$file" ]; then
+        g++ "$file" -o "${file%.*}"
+    fi
+done
+for file in /users/ses/rs_14/git/InternProject/dir3/*.cpp; do
+    if [ -f "$file" ]; then
+        g++ "$file" -o "${file%.*}"
+    fi
+done
+for file in /users/ses/rs_14/git/InternProject/dir4/*.cpp; do
+    if [ -f "$file" ]; then
+        g++ "$file" -o "${file%.*}"
+    fi
+done
+for file in /users/ses/rs_14/git/InternProject/dir5/*.cpp; do
+    if [ -f "$file" ]; then
+        g++ "$file" -o "${file%.*}"
+    fi
+done
+for file in /users/ses/rs_14/git/InternProject/dir6/*.cpp; do
+    if [ -f "$file" ]; then
+        g++ "$file" -o "${file%.*}"
+    fi
+done
+
+# g++ basic.cpp -o basic.out
+
+# Compile new.cpp
+# gcc new.c -o new.out
+
